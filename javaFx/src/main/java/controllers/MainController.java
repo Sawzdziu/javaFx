5500187effadfd76696697002c82f0d84d7af61a
Sawzdziu <piotr.sawzdargo@gmail.com>
2018-08-19 23:13:41 +0200

    -added spring context -added views

--- javaFx/src/main/java/controllers/MainController.java
+++ javaFx/src/main/java/controllers/MainController.java
@@ -1,11 +1,24 @@
 package controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.layout.VBox;
+import lombok.Getter;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class MainController {
 
+    @FXML
+    @Getter
+    private ListViewController listViewController;
 
+    @FXML
+    @Getter
+    private ToolBarController toolBarController;
+
+    @FXML
+    @Getter
+    private VBox mainVBox;
 
 }
 
